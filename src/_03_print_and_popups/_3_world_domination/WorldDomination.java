0dce4c000f5224336d6b6508adac44adee8af445
root <root@26f33f61fc97>
2020-06-19 18:53:29 +0000

    Coding-Last Day

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,6 +8,8 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
+		
+		JOptionPane.showInputDialog("Do you know how to write code?");
 
 		// 2. If they say "yes", tell them they will rule the world.
 
