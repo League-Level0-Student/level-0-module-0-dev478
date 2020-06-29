d0cb017d46b32bae709138a0acd9f6d2d37d2b66
root <root@72d5ec0e6f0d>
2020-06-29 18:34:28 +0000

    Day 1

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,18 +1,27 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+				String ans = JOptionPane.showInputDialog(null, "What goes on four legs in the morning, two legs at noon, and three legs in the evening?" );
+				System.out.println("your answer is:" + ans);
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if(ans.equalsIgnoreCase("man")) {
+					System.out.println("Your answer is correct!");
+					score++;
+				}
+				else {
+					System.out.println("Sorry, your answer is incorrect.");
+				}
 				// 4.  if the user's answer was correct, add one to their score 
-		
+		System.out.println("score is: " + score);
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
