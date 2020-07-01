d985d79675b47272672f874be461b61e88de7dbd
root <root@20a37cc25f15>
2020-07-01 18:31:03 +0000

    Day3

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,37 +2,63 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
+	public static void main(String[] args) {
 	// 1. Make a main method that includes everything below
-public static void main(String[] args) {
+		Random generator = new Random();    //This will be used below to make a random number. 
 	
-}
-	
-		Random ran = new Random();    //This will be used below to make a random number. 
-		
+		System.out.println();
+
+
 		// 2. Make a variable that will hold a random whole number
-	
+	int variable = generator.nextInt(4);
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
 	
-		// 3. Print your variable to the console
-	
+		// . Print your variable to the console
+	System.out.println(variable);
 		// 4. Get the user to enter something that they think is awesome
-	
+	String ans = JOptionPane.showInputDialog("Enter an item that you think is awesome.");
 		// 5. If your variable is  0
 	
+	if(ans.contentEquals("")) {
+		JOptionPane.showMessageDialog(null, "HOw DArE YoU Not ENTer ANYTHIng!!!!!!");
+	}
+	
+	if(ans.charAt(0)<64) {
+		JOptionPane.showMessageDialog(null, "DO NoT ansWER A NuMBer!!!!!");
+		
+		
+	}
+	if(variable==0) {
+		JOptionPane.showMessageDialog(null, ans + " is awesome!");
+	}
 			// -- tell the user whatever they entered is awesome!
 	
 		// 6. If your variable is  1
-	
+	if(variable==1) {
+		JOptionPane.showMessageDialog(null, ans + " is ok.");
+	}
 			// -- tell the user whatever they entered is ok.
 	
 		// 7. If your variable is  2
-	
+	if(variable==2) {
+			
+		JOptionPane.showMessageDialog(null, ans + " is boring.");
+	}
 			// -- tell the user whatever they entered is boring.
 	
-		// 8. If your variable is  3
+		
+			
+		
+if(variable==3) {
+	JOptionPane.showMessageDialog(null, ans + "is horrible.");
 	
-			// -- invent your own message to give to the user (be nice).
+}
+			// -- tell the user whatever they entered is horrible.
 
 }
+}
+
